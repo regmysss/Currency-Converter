837a5e9b2628bc91157a82abaf898b6ca4b559d0
regmysss <kabanigor3@gmail.com>
2023-11-28 10:47:03 +0200

    link course

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
+//https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2022 API course
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
   State<Home> createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
